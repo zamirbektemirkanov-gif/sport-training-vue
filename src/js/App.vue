9174c1ecfcb51bae354f945a4c48e63363e2f5a5
Zamir <zamir@inbox.ru>
2026-03-26 18:31:25 +1100

    first commit

---
+++ src/js/App.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="container">
+        <Nav />
+        <Head />
+        <Courses :data="courses" />
+    </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+import Head from './components/Head.vue'
+import Courses from './components/Courses.vue'
+
+export default {
+    components: { Nav, Head, Courses },
+    data() {
+        return {
+            courses: [
+                {'title': 'Кардио', 'cl': '250 калорий', 'time': '58:24', 'img': 'cardio.png'},
+                {'title': 'Растяжка', 'cl': '250 калорий', 'time': '58:24', 'img': 'stretch.png'},
+                {'title': 'Йога', 'cl': '250 калорий', 'time': '58:24', 'img': 'joga.png'},
+                {'title': 'Бег', 'cl': '250 калорий', 'time': '58:24', 'img': 'run.png'},
+                {'title': 'Гантели', 'cl': '250 калорий', 'time': '58:24', 'img': 'metal.png'},
+                {'title': 'На улице', 'cl': '250 калорий', 'time': '58:24', 'img': 'street.png'},
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+$mainColor: #000000;
+$textColor: #fff;
+$additionColor: #25AB75;
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+
+::selection {
+    background: $additionColor;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    background: $mainColor;
+    color: $textColor;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 1em;
+}
+
+a {
+    color: #ABABAB;
+    text-decoration: none;
+
+    &:hover {
+        color: #c7c7c7;
+    }
+}
+
+.container {
+    @media(min-width: 1500px) {
+        width: 1300px;
+        margin: 0 auto;
+    }
+    @media(max-width: 1500px) and (min-width: 900px) {
+        width: 80%;
+        margin: 0 10%;
+    }
+    @media(max-width: 900px) {
+        width: 90%;
+        margin: 0 5%;
+    }
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/js/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: [
+                    {
+                        loader: 'vue-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        port: 9000,
+        compress: true,
+        hot: true,
+        static: {
+            directory: path.join(__dirname, 'dist')
+        }
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
